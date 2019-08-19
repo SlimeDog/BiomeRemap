8eeb7ab9a75c4af450f66f2b05e03957e0b992be
Mart Ratas <mart.ratas@gmail.com>
2019-08-20 00:05:20 +0300

    Removed populator (for now at least). Made the debug messages go to a separate file.

--- src/main/java/me/ford/biomeremap/BiomeRemap.java
+++ src/main/java/me/ford/biomeremap/BiomeRemap.java
@@ -1,12 +1,18 @@
 package me.ford.biomeremap;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.logging.Logger;
 
-import org.bukkit.World;
+//import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import me.ford.biomeremap.commands.BiomeRemapCommand;
-import me.ford.biomeremap.populator.MappingPopulator;
+//import me.ford.biomeremap.populator.MappingPopulator;
 //import me.ford.biomeremap.listeners.ChunkListener;
 import me.ford.biomeremap.settings.Messages;
 import me.ford.biomeremap.settings.Settings;
@@ -18,12 +24,13 @@ public class BiomeRemap extends JavaPlugin {
 	@Override
 	public void onEnable() {
 //		getServer().getPluginManager().registerEvents(new ChunkListener(this), this);
+		saveDefaultConfig();
 		settings = new Settings(this);
 		messages = new Messages(this);
-		MappingPopulator populator = new MappingPopulator();
-		for (World world : getServer().getWorlds()) {
-			world.getPopulators().add(populator);
-		}
+//		MappingPopulator populator = new MappingPopulator();
+//		for (World world : getServer().getWorlds()) {
+//			world.getPopulators().add(populator);
+//		}
 		// commands
 		getCommand("biomeremap").setExecutor(new BiomeRemapCommand(this));
 	}
@@ -46,8 +53,19 @@ public class BiomeRemap extends JavaPlugin {
 		return JavaPlugin.getPlugin(BiomeRemap.class).getLogger();
 	}
 	
-	public static void debug(String msg) {
-		logger().info("[DEBUG]" + msg);
+	public static void debug(String msg) {	     
+	    BufferedWriter writer;
+		try {
+			writer = new BufferedWriter(
+			                            new FileWriter(JavaPlugin.getPlugin(BiomeRemap.class).getDataFolder().getAbsolutePath() + File.separatorChar + "debug.log", true)  //Set true for append mode
+			                        );
+		    writer.newLine();   //Add new line
+		    writer.write(new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new Date()) + ": " + msg);
+		    writer.close();
+		} catch (IOException e) {
+			logger().warning("Unable to save debug logging data!");
+		} 
+//		logger().info("[DEBUG]" + msg);
 	}
 
 }
