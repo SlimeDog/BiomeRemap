db37069571da60c9f4e043e33ab92dbdbd172ec6
Mart Ratas <mart.ratas@gmail.com>
2019-08-24 09:32:43 +0300

    Made the biome ID bit be non-version specific through reflection. Should work with future versions if NMS doesn't change considerably.

--- src/main/java/me/ford/biomeremap/hooks/PlaceholderAPIHook.java
+++ src/main/java/me/ford/biomeremap/hooks/PlaceholderAPIHook.java
@@ -1,19 +1,17 @@
 package me.ford.biomeremap.hooks;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
 
 import org.bukkit.block.Biome;
-import org.bukkit.craftbukkit.v1_14_R1.block.CraftBlock;
 import org.bukkit.entity.Player;
 
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import me.ford.biomeremap.BiomeRemap;
-import net.minecraft.server.v1_14_R1.BiomeBase;
-import net.minecraft.server.v1_14_R1.Biomes;
-import net.minecraft.server.v1_14_R1.IRegistry;
-import net.minecraft.server.v1_14_R1.RegistryMaterials;
 
 public class PlaceholderAPIHook extends PlaceholderExpansion {
 	private final BiomeRemap br;
@@ -26,17 +24,97 @@ public class PlaceholderAPIHook extends PlaceholderExpansion {
 	}
 	
 	private void mapBiomesWithReflection() { // TODO - there this is version specific
-		for (Field f : Biomes.class.getDeclaredFields()) {
+		String version = br.getServer().getClass().getPackage().getName().split("\\.")[3];
+		Class<?> biomesClass;
+		try {
+			biomesClass = Class.forName("net.minecraft.server." + version + ".Biomes");
+		} catch (ClassNotFoundException e1) {
+			br.getLogger().log(Level.SEVERE, "Issue while getting NMS Biomes class:", e1);
+			return;
+		}
+		Class<?> craftBlockClass;
+		try {
+			craftBlockClass = Class.forName("org.bukkit.craftbukkit." + version + ".block.CraftBlock");
+		} catch (ClassNotFoundException e1) {
+			br.getLogger().log(Level.SEVERE, "Issue while getting CraftBlock class:", e1);
+			return;
+		}
+		Class<?> biomeBaseClass;
+		try {
+			biomeBaseClass = Class.forName("net.minecraft.server." + version + ".BiomeBase");
+		} catch (ClassNotFoundException e1) {
+			br.getLogger().log(Level.SEVERE, "Issue while getting NMS BiomeBase class:", e1);
+			return;
+		}
+		Class<?> iRegistryClass;
+		try {
+			iRegistryClass = Class.forName("net.minecraft.server." + version + ".IRegistry");
+		} catch (ClassNotFoundException e1) {
+			br.getLogger().log(Level.SEVERE, "Issue while getting NMS IRegistry class:", e1);
+			return;
+		}
+		Class<?> registryMaterialsClass;
+		try {
+			registryMaterialsClass = Class.forName("net.minecraft.server." + version + ".RegistryMaterials");
+		} catch (ClassNotFoundException e1) {
+			br.getLogger().log(Level.SEVERE, "Issue while getting NMS RegistryMaterials class:", e1);
+			return;
+		}
+		Method biomeBaseToBiomeField;
+		try {
+			biomeBaseToBiomeField = craftBlockClass.getDeclaredMethod("biomeBaseToBiome", biomeBaseClass);
+		} catch (NoSuchMethodException | SecurityException e1) {
+			br.getLogger().log(Level.SEVERE, "Issue while getting CraftBlock#biomeBaseToBiome method:", e1);
+			return;
+		}
+		Field biomeField;
+		try {
+			biomeField = iRegistryClass.getDeclaredField("BIOME");
+		} catch (NoSuchFieldException | SecurityException e1) {
+			br.getLogger().log(Level.SEVERE, "Issue while getting IRegistry.BIOME field:", e1);
+			return;
+		}
+		Method getIdMethod = null;
+		for (Method method : registryMaterialsClass.getMethods()) {
+			if (!method.getName().equals("a")) continue;
+			Class<?>[] types = method.getParameterTypes();
+			if (method.getReturnType() != int.class || types.length != 1 || types[0] != Object.class) continue;
+			getIdMethod = method;
+		}
+		if (getIdMethod == null) {
+			br.getLogger().log(Level.SEVERE, "Issue while getting NMS BiomeBase#a method");
+			return;
+		}
+		Object biomeRegistry;
+		try {
+			biomeRegistry = biomeField.get(null);
+		} catch (IllegalArgumentException | IllegalAccessException e1) {
+			br.getLogger().log(Level.SEVERE, "Issue while getting NMS IRegistry.BIOME field", e1);
+			return;
+		}
+		for (Field f : biomesClass.getDeclaredFields()) {
 			if (f.getName().length() > 1) { // ignore the OCEAN duplicate Biomes.b
-				BiomeBase base;
+				Object base;
 				try {
-					base = (BiomeBase) f.get(null);
+					base = f.get(null);
 				} catch (IllegalArgumentException | IllegalAccessException e) {
-					e.printStackTrace();
+					br.getLogger().log(Level.SEVERE, "Issue while getting field of NMS Biomes class:", e);
 					continue;
 				}
-				Biome biome = CraftBlock.biomeBaseToBiome(base);
-				int nr = ((RegistryMaterials<BiomeBase>) IRegistry.BIOME).a(base);
+				Biome biome;
+				try {
+					biome = (Biome) biomeBaseToBiomeField.invoke(null, base);
+				} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+					br.getLogger().log(Level.SEVERE, "Issue while invoking CraftBlock#biomeBaseToBiome method:", e);
+					continue;
+				}
+				int nr;
+				try {
+					nr = (int) getIdMethod.invoke(biomeRegistry, base);
+				} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+					br.getLogger().log(Level.SEVERE, "Issue while getting invoking RegistryMaterials#a method:", e);
+					continue;
+				}
 				byBiome.put(biome, nr);
 			}
 		}
