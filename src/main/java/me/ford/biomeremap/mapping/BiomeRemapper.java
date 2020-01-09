c21c4b45eae5b69986af33907519d9723e59d4a7
Mart Ratas <mart.ratas@gmail.com>
2020-01-09 13:21:28 +0200

    Attempting to do the biome checking in an async thread instead

--- src/main/java/me/ford/biomeremap/mapping/BiomeRemapper.java
+++ src/main/java/me/ford/biomeremap/mapping/BiomeRemapper.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 import org.bukkit.Chunk;
-import org.bukkit.World;
+import org.bukkit.ChunkSnapshot;
 import org.bukkit.block.Biome;
 
 import me.ford.biomeremap.BiomeRemap;
@@ -39,46 +39,42 @@ public class BiomeRemapper {
 		return remapChunk(chunk, debug, map);
 	}
 	
-	public long remapChunk(Chunk chunk, boolean debug, BiomeMap map) {
+	public long remapChunk(final Chunk chunk, boolean debug, final BiomeMap map) {
 		long start = System.currentTimeMillis();
 		if (debug) BiomeRemap.debug("Looking for biomes to remap (SYNC) in chunk:" + chunk.getX() + "," + chunk.getZ() + "...");
-		World world = chunk.getWorld();
 		if (map == null) return 0;
-		if (debug) BiomeRemap.debug(world.getName() + "->Mapping " + map.getName());
-		int chunkX = chunk.getX() * 16;
-		int chunkZ = chunk.getZ() * 16;
+		if (debug) BiomeRemap.debug(chunk.getWorld().getName() + "->Mapping " + map.getName());
 		Map<Integer, BiomeChoice> toChange = new HashMap<>();
 		Map<Biome, Biome> changes = new HashMap<>();
-		for (int x = 0; x < 16; x++) { // the grid only has 4 sections per horizontal axis
-			for (int z = 0; z < 16; z++) { // the grid only has 4 sections per horizontal axis
-				Biome cur = world.getBiome(chunkX + x, 0, chunkZ + z); // TODO - in the future, we might need to change/get biomes at other y values, but for now only y=0 has an effect
-				Biome req = map.getBiomeFor(cur);
-				if (req != null) {
-					int key = x >> 2 << 2 | z >> 2;
-					if (!toChange.containsKey(key)) {
-						toChange.put(key, new BiomeChoice());
+		ChunkSnapshot snapshot = chunk.getChunkSnapshot(false, true, false);
+		br.getServer().getScheduler().runTaskAsynchronously(br, () -> {
+			for (int x = 0; x < 16; x++) { // the grid only has 4 sections per horizontal axis
+				for (int z = 0; z < 16; z++) { // the grid only has 4 sections per horizontal axis
+					Biome cur = snapshot.getBiome(x, 0, z); // TODO - in the future, we might need to change/get biomes at other y values, but for now only y=0 has an effect
+					Biome req = map.getBiomeFor(cur);
+					if (req != null) {
+						int key = x >> 2 << 2 | z >> 2;
+						if (!toChange.containsKey(key)) {
+							toChange.put(key, new BiomeChoice());
+						}
+						toChange.get(key).addChoice(req);
 					}
-					toChange.get(key).addChoice(req);
-				}
-				if (!changes.containsKey(cur)) {
-					changes.put(cur, req);
+					if (!changes.containsKey(cur)) {
+						changes.put(cur, req);
+					}
 				}
 			}
-		}
-		if (!toChange.isEmpty()) {
-			if (debug) BiomeRemap.debug("Found:" + changes);
-			doMapping(chunk, toChange, debug);
-		}
+			if (!toChange.isEmpty()) {
+				if (debug) BiomeRemap.debug("Found:" + changes);
+				br.getServer().getScheduler().runTask(br, () -> doMapping(chunk, toChange, debug));
+			}
+		});
 		return System.currentTimeMillis() - start;
 	}
 	
 	private void doMapping(Chunk chunk, Map<Integer, BiomeChoice> toChange, boolean debug) {
 		if (debug) BiomeRemap.debug("Remapping biomes");
-		// World world = chunk.getWorld();
-		// int startX = chunk.getX() * 16;
-		// int startZ = chunk.getZ() * 16;
 		for (Entry<Integer, BiomeChoice> entry : toChange.entrySet()) {
-			// world.setBiome(startX + x, 0, startZ + z, entry.getValue()); // TODO - in the future, we might need to change/get biomes at other y values, but for now only y=0 has an effect
 			changeBiomeInChunk(chunk, entry.getKey(), entry.getValue().choose());
 		}
 	}
