bc1a194ab03442bb1445edf661cc727b4af3e48b
Mart drives_a_ford Ratas <mart.ratas@gmail.com>
2021-09-25 07:54:50 +0300

    Moved to jupiter test environment (previous tests were not run on packaging anyway)

--- src/test/java/me/ford/biomeremap/MainTests.java
+++ src/test/java/me/ford/biomeremap/MainTests.java
@@ -1,34 +1,34 @@
 package me.ford.biomeremap;
 
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import be.seeseemelk.mockbukkit.MockBukkit;
 
 public class MainTests {
 	private BiomeRemap plugin;
 
-	@Before
+	@BeforeEach
 	public void setUp() {
 		MockBukkit.mock();
 		plugin = MockBukkit.load(BiomeRemap.class);
 	}
 
-	@After
+	@AfterEach
 	public void tearDown() {
 		MockBukkit.unmock();
 	}
 
 	@Test
 	public void settings_not_null() {
-		Assert.assertNotNull(plugin.getSettings());
+		Assertions.assertNotNull(plugin.getSettings());
 	}
 
 	@Test
 	public void messages_not_null() {
-		Assert.assertNotNull(plugin.getMessages());
+		Assertions.assertNotNull(plugin.getMessages());
 	}
 
 }
